f859af33c16f261901e168efd2f226d4aa002127
usa4060 <cocoa9805@naver.com>
2022-01-17 22:34:15 +0900

    chapter01_8ex

--- swift_ex/swift_ex/chapter01_8ex.swift
+++ swift_ex/swift_ex/chapter01_8ex.swift
@@ -1,8 +1,29 @@
-//
-//  chapter01_8ex.swift
-//  swift_ex
-//
-//  Created by admin on 2022/01/17.
-//
+let someInteger = 100
 
-import Foundation
+if someInteger < 100 {
+    print("100 미만")
+} else if(someInteger > 100){
+    print("100 초과")
+} else{
+    print("100")
+}
+
+
+switch someInteger{
+case 0:
+    print("zero")
+case 1..<100:
+    print("1~99")
+case 101...Int.max:
+    print("over 100")
+default:
+    print("unknown")
+}
+
+switch "yongin"{
+case "yongin":
+    print("yes")
+default:
+    print("unknown")
+}
+
