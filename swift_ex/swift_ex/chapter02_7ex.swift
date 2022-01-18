c3bdaf8dd5ab1f8d93f2153a1d4afd3c08c07254
usa4060 <cocoa9805@naver.com>
2022-01-18 14:14:43 +0900

    chapter02_7ex

---
+++ swift_ex/swift_ex/chapter02_7ex.swift
@@ -0,0 +1,54 @@
+struct Money {
+    // 프로퍼티 감시자 사용
+    var currencyRate: Double = 1100 {
+        willSet(newRate) {
+            print("환율이 \(currencyRate)에서 \(newRate)으로 변경될 예정입니다")
+        }
+        
+        didSet(oldRate) {
+            print("환율이 \(oldRate)에서 \(currencyRate)으로 변경되었습니다")
+        }
+    }
+
+    // 프로퍼티 감시자 사용
+    var dollar: Double = 0 {
+        // willSet의 암시적 매개변수 이름 newValue
+        willSet {
+            print("\(dollar)달러에서 \(newValue)달러로 변경될 예정입니다")
+        }
+        
+        // didSet의 암시적 매개변수 이름 oldValue
+        didSet {
+            print("\(oldValue)달러에서 \(dollar)달러로 변경되었습니다")
+        }
+    }
+
+    // 연산 프로퍼티
+    var won: Double {
+        get {
+            return dollar * currencyRate
+        }
+        set {
+            dollar = newValue / currencyRate
+        }
+        
+        /* 프로퍼티 감시자와 연산 프로퍼티 기능을 동시에 사용할 수 없습니다
+        willSet {
+            
+        }
+         */
+    }
+}
+
+var moneyInMyPocket: Money = Money()
+
+// 환율이 1100.0에서 1150.0으로 변경될 예정입니다
+moneyInMyPocket.currencyRate = 1150
+// 환율이 1100.0에서 1150.0으로 변경되었습니다
+
+// 0.0달러에서 10.0달러로 변경될 예정입니다
+moneyInMyPocket.dollar = 10
+// 0.0달러에서 10.0달러로 변경되었습니다
+
+print(moneyInMyPocket.won)
+// 11500.0
