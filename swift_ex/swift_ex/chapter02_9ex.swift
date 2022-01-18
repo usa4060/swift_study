47d0c57b6352514461ba7ebe1e827c786505a9f8
usa4060 <cocoa9805@naver.com>
2022-01-18 15:35:45 +0900

    chapter02_9ex

---
+++ swift_ex/swift_ex/chapter02_9ex.swift
@@ -0,0 +1,120 @@
+class PersonA {
+    // 모든 저장 프로퍼티에 기본값 할당
+    var name: String = "unknown"
+    var age: Int = 0
+    var nickName: String = "nick"
+}
+
+// 인스턴스 생성
+let jason: PersonA = PersonA()
+
+// 기본값이 인스턴스가 지녀야 할 값과 맞지 않다면
+// 생성된 인스턴스의 프로퍼티에 각각 값 할당
+jason.name = "jason"
+jason.age = 30
+jason.nickName = "j"
+
+class PersonB {
+    var name: String
+    var age: Int
+    var nickName: String
+    
+    // 이니셜라이저
+    init(name: String, age: Int, nickName: String) {
+        self.name = name
+        self.age = age
+        self.nickName = nickName
+    }
+}
+
+let hana: PersonB = PersonB(name: "hana", age: 20, nickName: "하나")
+
+class PersonC {
+    var name: String
+    var age: Int
+    var nickName: String?
+
+    convenience init(name: String, age: Int, nickName: String) {
+        self.init(name: name, age: age)
+        self.nickName = nickName
+    }
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let jenny: PersonC = PersonC(name: "jenny", age: 10)
+let mike: PersonC = PersonC(name: "mike", age: 15, nickName: "m")
+
+class Puppy {
+    var name: String
+    var owner: PersonC!
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func goOut() {
+        print("\(name)가 주인 \(owner.name)와 산책을 합니다")
+    }
+}
+
+let happy: Puppy = Puppy(name: "happy")
+// 강아지는 주인없이 산책하면 안돼요!
+//happy.goOut() // 주인이 없는 상태라 오류 발생
+happy.owner = jenny
+happy.goOut()
+// happy가 주인 jenny와 산책을 합니다
+
+class PersonD {
+    var name: String
+    var age: Int
+    var nickName: String?
+    
+    init?(name: String, age: Int) {
+        if (0...120).contains(age) == false {
+            return nil
+        }
+        
+        if name.count == 0 {
+            return nil
+        }
+        
+        self.name = name
+        self.age = age
+    }
+}
+
+//let john: PersonD = PersonD(name: "john", age: 23)
+let john: PersonD? = PersonD(name: "john", age: 23)
+let joker: PersonD? = PersonD(name: "joker", age: 123)
+let batman: PersonD? = PersonD(name: "", age: 10)
+
+print(joker) // nil
+print(batman) // nil
+
+class PersonE {
+    var name: String
+    var pet: Puppy?
+    var child: PersonC
+    
+    init(name: String, child: PersonC) {
+        self.name = name
+        self.child = child
+    }
+    
+    // 인스턴스가 메모리에서 해제되는 시점에 자동 호출
+    deinit {
+        if let petName = pet?.name {
+            print("\(name)가 \(child.name)에게 \(petName)를 인도합니다")
+            self.pet?.owner = child
+        }
+    }
+}
+
+var donald: PersonE? = PersonE(name: "donald", child: jenny)
+donald?.pet = happy
+donald = nil // donald 인스턴스가 더이상 필요없으므로 메모리에서 해제됩니다
+// donald가 jenny에게 happy를 인도합니다
