8bd15a4cb51f8e7c78980e1d986981441eb7599e
usa4060 <cocoa9805@naver.com>
2022-01-18 13:22:52 +0900

    chapter02_6ex

---
+++ swift_ex/swift_ex/chapter02_6ex.swift
@@ -0,0 +1,94 @@
+struct Student {
+    
+    // 인스턴스 저장 프로퍼티
+    var name: String = ""
+    var `class`: String = "Swift"
+    var koreanAge: Int = 0
+    
+    // 인스턴스 연산 프로퍼티
+    var westernAge: Int {
+        get {
+            return koreanAge - 1
+        }
+        
+        set(inputValue) {
+            koreanAge = inputValue + 1
+        }
+    }
+    
+    // 타입 저장 프로퍼티
+    static var typeDescription: String = "학생"
+    
+    /*
+    // 인스턴스 메서드
+    func selfIntroduce() {
+        print("저는 \(self.class)반 \(name)입니다")
+    }
+     */
+    
+    // 읽기전용 인스턴스 연산 프로퍼티
+    // 간단히 위의 selfIntroduce() 메서드를 대체할 수 있습니다
+    var selfIntroduction: String {
+        get {
+            return "저는 \(self.class)반 \(name)입니다"
+        }
+    }
+        
+    /*
+     // 타입 메서드
+     static func selfIntroduce() {
+     print("학생타입입니다")
+     }
+     */
+    
+    // 읽기전용 타입 연산 프로퍼티
+    // 읽기전용에서는 get을 생략할 수 있습니다
+    static var selfIntroduction: String {
+        return "학생타입입니다"
+    }
+}
+
+// 타입 연산 프로퍼티 사용
+print(Student.selfIntroduction)
+// 학생타입입니다
+
+// 인스턴스 생성
+var yongin: Student = Student()
+yongin.koreanAge = 10
+
+// 인스턴스 저장 프로퍼티 사용
+yongin.name = "yongin"
+print(yongin.name)
+// yagom
+
+// 인스턴스 연산 프로퍼티 사용
+print(yongin.selfIntroduction)
+// 저는 Swift반 yagom입니다
+
+print("제 한국나이는 \(yongin.koreanAge)살이고, 미쿡나이는 \(yongin.westernAge)살입니다.")
+// 제 한국나이는 10살이고, 미쿡나이는 9살입니다.
+
+struct Money {
+    var currencyRate: Double = 1100
+    var dollar: Double = 0
+    var won: Double {
+        get {
+            return dollar * currencyRate
+        }
+        set {
+            dollar = newValue / currencyRate
+        }
+    }
+}
+
+var moneyInMyPocket = Money()
+
+moneyInMyPocket.won = 11000
+
+print(moneyInMyPocket.won)
+// 11000
+
+moneyInMyPocket.dollar = 10
+
+print(moneyInMyPocket.won)
+// 11000
