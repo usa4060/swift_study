f4896599ab4e299a4426d03cbe400ba5a82356c0
usa4060 <cocoa9805@naver.com>
2022-01-18 09:54:12 +0900

    chapter02_2ex

---
+++ swift_ex/swift_ex/chapter02_2ex.swift
@@ -0,0 +1,32 @@
+class Student {
+    // 가변 프로퍼티
+    var name: String = "unknown"
+
+    // 키워드도 `로 묶어주면 이름으로 사용할 수 있습니다
+    var `class`: String = "Swift"
+    
+    // 타입 메서드
+    class func selfIntroduce() {
+        print("학생타입입니다")
+    }
+    
+    // 인스턴스 메서드
+    // self는 인스턴스 자신을 지칭하며, 몇몇 경우를 제외하고 사용은 선택사항입니다
+    func selfIntroduce() {
+        print("저는 \(self.class)반 \(name)입니다")
+    }
+}
+
+// 타입 메서드 사용
+Student.selfIntroduce() // 학생타입입니다
+
+// 인스턴스 생성
+var yagom: Student = Student()
+yagom.name = "yagom"
+yagom.class = "스위프트"
+yagom.selfIntroduce()   // 저는 스위프트반 yagom입니다
+
+// 인스턴스 생성
+let jina: Student = Student()
+jina.name = "jina"
+jina.selfIntroduce() // 저는 Swift반 jina입니다
